444e7245a2b2f45be201d0eef01c45a8987a8dfb
Prism411 <jaderlouis@proton.me>
2024-01-10 16:50:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
